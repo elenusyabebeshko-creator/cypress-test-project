f578880c6d1cacce2b8e5786d2c196a4f4127061
DESKTOP-D0AJ14D\Elena <elena.bebeshko@xmlshop.com>
2026-08-19 23:50:39 +0300

    HW 19.1 Cypress queries — header and footer elements tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,13 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-    allowCypressEnv: false,
-
     e2e: {
-        //baseUrl: 'http://localhost:3000', //щоб у тестах не писати повний URL щоразу;
-        viewportWidth: 1280, //фіксований розмір вікна браузера;
-        viewportHeight: 720, //фіксований розмір вікна браузера;
-        defaultCommandTimeout: 8000, //збільшений таймаут очікування елементів;
-        video: false, //вимкнено запис відео прогонів (щоб не засмічувати диск);
-        screenshotOnRunFailure: true, //скріншот при падінні тесту.
+        baseUrl: 'https://qauto.forstudy.space',
+        viewportWidth: 1280,
+        viewportHeight: 720,
+        defaultCommandTimeout: 10000,
+        video: false,
+        screenshotOnRunFailure: true,
         // eslint-disable-next-line no-unused-vars
         setupNodeEvents(on, config) {
             // тут можна підключати плагіни/події
