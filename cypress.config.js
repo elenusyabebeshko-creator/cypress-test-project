1771319672206da6f362f72bf24e6e81a94724d4
DESKTOP-D0AJ14D\Elena <elena.bebeshko@xmlshop.com>
2026-08-28 20:16:10 +0300

    modified:   cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+    allowCypressEnv: false,
     e2e: {
         baseUrl: 'https://qauto.forstudy.space',
         viewportWidth: 1280,
